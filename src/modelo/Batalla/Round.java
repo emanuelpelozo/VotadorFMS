c5c0bcd8cd2c296fdaa8cc911286e8eea37a6b2c
emanuelpelozo <emanuel.pelozo@outlook.com>
2019-11-22 16:32:43 -0300

    Avance con round y pruebas

--- src/modelo/Batalla/Round.java
+++ src/modelo/Batalla/Round.java
@@ -7,17 +7,17 @@ import java.util.ArrayList;
 public class Round {
 
     private ArrayList<Integer> puntajes;
-    private int cantRounds;
+    private int cantEntradas;
 
-    public Round(int cantRounds){
-        this.cantRounds = cantRounds;
+    public Round(int cantEntradas){
+        this.cantEntradas = cantEntradas;
         this.inicializarPuntajes();
     }
 
     private void inicializarPuntajes() {
 
         puntajes = new ArrayList();
-        for(int i = 0; i<this.cantRounds; i++){
+        for(int i = 0; i<this.cantEntradas; i++){
 
             puntajes.add(i, 0);
         }
@@ -29,16 +29,17 @@ public class Round {
 
     public void votarPatron(int nroPatron, int puntaje) {
 
-        this.puntajes.add( nroPatron - 1, puntaje);
+        this.puntajes.set( nroPatron - 1, puntaje);
     }
 
     public int getPuntajeAcumulado(){
         int puntajeAcumulado = 0;
 
-        for (int i = 0; i < this.cantRounds; i++) {
+        for (int i = 0; i < this.cantEntradas; i++) {
 
             puntajeAcumulado += this.puntajes.get(i);
 
+            System.out.println(i);
             System.out.println(this.puntajes.get(i));
         }
         return puntajeAcumulado;
--- src/test/RoundTest.java
+++ src/test/RoundTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import modelo.Batalla.Round;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,15 +8,116 @@ import static org.junit.Assert.*;
 
 public class RoundTest {
 
+    private Round round;
+    private int cantEntradas;
+
     @Before
     public void setUp() throws Exception {
+
+        this.cantEntradas = 5;
+        this.round = new Round(this.cantEntradas);
     }
 
     @Test
-    public void puntuarEntradaParaParticipante() {
+    public void puntajeAcumuladoDeRoundAlComenzarEsCero() {
+        int puntajeEsperado = 0;
+        assertEquals(puntajeEsperado, round.getPuntajeAcumulado());
+
     }
 
     @Test
-    public void getṔuntuacion() {
+    public void alVotarPrimeraEntradaSeActualizaElPuntajeAcumulado() {
+        int puntajeEsperado = 2;
+        int nroPatron = 1;
+
+        //Antes de votar el puntaje es cero
+        assertEquals(0, round.getPuntajeAcumulado());
+
+        //Despues de votar se actualiza el puntaje
+        round.votarPatron(nroPatron, puntajeEsperado);
+        assertEquals(puntajeEsperado, round.getPuntajeAcumulado());
+
     }
+
+    @Test
+    public void alPuntuarEntradaPorPrimeraVezSeInicializaSuValor(){
+        int puntajeEsperado = 4;
+        int nroPatron = 2;
+
+        //Antes de votar cero
+        assertEquals(0, round.getPuntajePatron(nroPatron));
+
+        //Despues de votar, se actualiza
+        round.votarPatron(nroPatron, puntajeEsperado);
+        assertEquals(puntajeEsperado, round.getPuntajePatron(nroPatron));
+
+    }
+    @Test
+    public void alPuntuarNuevamenteLaEntradaSeActualizaElValorDeLaEntrada(){
+
+        int puntajeAntiguo = 1;
+        int puntajeNuevo = 3;
+        int nroPatron = 3;
+
+        round.votarPatron(nroPatron, puntajeAntiguo);
+
+        //Actualizamos el valor
+        round.votarPatron(nroPatron, puntajeNuevo);
+
+        assertEquals(puntajeNuevo, round.getPuntajePatron(nroPatron));
+
+    }
+
+    @Test
+    public void alPuntuarNuevamenteUnaEntradaSeActualizaElPuntajeAcumulado(){
+        int puntajeAntiguo = 1;
+        int puntajeNuevo = 3;
+        int nroPatron = 3;
+
+        round.votarPatron(nroPatron, puntajeAntiguo);
+
+        //Actualizamos el valor
+        round.votarPatron(nroPatron, puntajeNuevo);
+        assertEquals(puntajeNuevo, round.getPuntajeAcumulado());
+
+    }
+
+    @Test
+    public void alPuntuarPorPrimeraVezDosEntradaDistintasSeAcumulaElPuntajeTotal(){
+        int puntaje1 = 1;
+        int puntaje2 = 3;
+        int nroPatron1 = 3;
+        int nroPatron2 = 2;
+
+        round.votarPatron(nroPatron1, puntaje1);
+        round.votarPatron(nroPatron2, puntaje2);
+
+        assertEquals(puntaje1 + puntaje2, round.getPuntajeAcumulado());
+    }
+
+
+    @Test
+    public void alTenerVotadosTresPatronesYLuegoModificarUnoSeActualizaElPuntajeAcumulado(){
+
+        int puntaje1 = 1;
+        int puntaje2 = 3;
+        int puntaje3 = 1;
+        int nuevoPuntaje3 = 4;
+
+        int nroPatron1 = 3;
+        int nroPatron2 = 2;
+        int nroPatron3 = 1;
+
+        round.votarPatron(nroPatron1, puntaje1);
+        round.votarPatron(nroPatron2, puntaje2);
+        round.votarPatron(nroPatron3, puntaje3);
+
+        //El puntaje acumulado es la suma de todos los puntajes
+        assertEquals(puntaje1+puntaje2+puntaje3, round.getPuntajeAcumulado());
+
+        //Al modificar un puntaje se actualiza el puntaje acumulado
+        round.votarPatron(nroPatron3, nuevoPuntaje3);
+        assertEquals( puntaje1+puntaje2+nuevoPuntaje3, round.getPuntajeAcumulado());
+    }
+
 }
