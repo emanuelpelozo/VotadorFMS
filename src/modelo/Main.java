08631c262ae935c88981e83f6e65c23646dfed86
Emanuel Pelozo <emanuel.pelozo@outlook.com>
2020-02-14 00:26:05 -0300

    Comienzo con la vista

--- src/modelo/Main.java
+++ src/modelo/Main.java
@@ -1,7 +1,12 @@
 package modelo;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -10,7 +15,11 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/vista/vistaPrincipal.fxml"));
+        primaryStage.setTitle("Formato FMS");
+        primaryStage.setScene(new Scene(root, 500, 350));
+        primaryStage.show();
 
     }
 }
